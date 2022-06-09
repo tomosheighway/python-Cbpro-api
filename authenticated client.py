e9fc9b9731c6615465bc491d077aa049e3c90e15
tomosheighway <tomosheighway@gmail.com>
2022-06-09 23:53:19 +0100

    changes

--- authenticated client.py
+++ authenticated client.py
@@ -9,24 +9,24 @@ passphrase = data[2]
 auth_client = cbpro.AuthenticatedClient(public_key, secret_key, passphrase)
 
 #limit buy
-print(auth_client.buy(price="10.0", size="10.1" ,order_type="limit", product_id="ETH-GBP"))     
+#print(auth_client.buy(price="10.0", size="10.1" ,order_type="limit", product_id="ETH-GBP"))     
 #market buy
-print(auth_client.buy(size="10", order_type= "market", product_id="ETH-GBP"))      
+#print(auth_client.buy(size="10", order_type= "market", product_id="ETH-GBP"))      
 
 #limit sell
-print(auth_client.sell(price="200000.0", size="0.1" ,order_type="limit", product_id="ETH-GBP"))  
+#print(auth_client.sell(price="200000.0", size="0.1" ,order_type="limit", product_id="ETH-GBP"))  
 #market sell
-print(auth_client.sell(size="10", order_type= "market", product_id="ETH-GBP"))  
+#print(auth_client.sell(size="10", order_type= "market", product_id="ETH-GBP"))  
 
 #alt methods of buy /sell 
-print(auth_client.place_limit_order(product_id="BTC-GBP", side="buy", price="10.00", size="1.0"))
-print(auth_client.place_market_order(product_id="BTC-GBP", side="buy", size="1.0"))
+#print(auth_client.place_limit_order(product_id="BTC-GBP", side="buy", price="10.00", size="1.0"))
+#print(auth_client.place_market_order(product_id="BTC-GBP", side="buy", size="1.0"))
 
 #canceling orders 
-print(auth_client.cancel_all(product_id="BTC-GBP"))
+#print(auth_client.cancel_all(product_id="BTC-GBP"))
 
 #getting orders
-print(auth_client.get_orders(product_id="BTC-ETH"))
+#print(auth_client.get_orders(product_id="BTC-ETH"))
 
 import time 
 sell_price = 300000
